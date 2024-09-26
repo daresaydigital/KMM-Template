964cb5d39f88188cc1676a259da2ba8bfda52c87
Yauheni <yauheni.slizh@knightec.se>
2024-09-26 10:08:16 +0200

    Added default configuration for KMM project with shared module and SwiftUi

---
+++ shared/build.gradle.kts
@@ -0,0 +1,126 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        androidMain.dependencies {
+
+
+
+
+
+            //di dependencies
+
+
+
+
+            //networking dependencies
+
+
+
+
+            //navigation dependencies
+
+
+
+
+            //view_models dependencies
+
+
+
+
+
+        }
+        commonMain.dependencies {
+
+
+
+
+
+            //di dependencies
+
+
+
+
+            //networking dependencies
+
+
+
+
+            //navigation dependencies
+
+
+
+
+            //view_models dependencies
+
+
+
+
+
+        }
+        iosMain.dependencies {
+
+
+
+
+
+            //di dependencies
+
+
+
+
+            //networking dependencies
+
+
+
+
+            //navigation dependencies
+
+
+
+
+            //view_models dependencies
+
+
+
+
+
+        }
+    }
+}
+
+android {
+    namespace = "co.daresay.kmmtemplate.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
