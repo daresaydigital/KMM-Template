00004dce9e2973034af8830fed9d039e5eb486e8
Yauheni <yauheni.slizh@knightec.se>
2024-09-27 10:42:45 +0200

    Added simple implementation of ColumnAdapter and Database implementation

--- shared/src/androidMain/kotlin/co/daresay/kmmtemplate/data/storage/driver/DatabaseDriverProvider.kt
+++ shared/src/androidMain/kotlin/co/daresay/kmmtemplate/data/storage/driver/DatabaseDriverProvider.kt
@@ -2,7 +2,6 @@ package co.daresay.kmmtemplate.data.storage.driver
 
 import android.content.Context
 
-//TODO replace this implementation after setup DI framework for providing dependencies
 actual object DatabaseDriverProvider {
     actual var driver: DatabaseDriverFactory? = null
 
--- shared/src/commonMain/kotlin/co/daresay/kmmtemplate/data/storage/driver/DatabaseDriverProvider.kt
+++ shared/src/commonMain/kotlin/co/daresay/kmmtemplate/data/storage/driver/DatabaseDriverProvider.kt
@@ -1,5 +1,9 @@
 package co.daresay.kmmtemplate.data.storage.driver
 
+/**
+ * Simple implementation for providing DatabaseDriverFactory. Replace it on direct creation driver
+ * factory after DI integration
+ */
 expect object DatabaseDriverProvider {
     var driver: DatabaseDriverFactory?
 }
