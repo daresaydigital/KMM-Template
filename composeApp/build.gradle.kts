d92fb3998bea24e84a0d147be069fbfff11da446
Yauheni <yauheni.slizh@knightec.se>
2024-10-03 10:44:24 +0200

    Base configuration of Koin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -22,6 +22,8 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.core)
+            implementation(libs.koin.android)
 
 
 
---
+++ composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate/di/modules/AndroidModule.kt
@@ -0,0 +1,7 @@
+package co.daresay.kmmtemplate.di.modules
+
+import org.koin.dsl.module
+
+val androidModule = module {
+
+}
