deee386db72376257001366097a62ff3e4a8b7d8
Yauheni <yauheni.slizh@knightec.se>
2024-10-07 11:00:40 +0200

    Updated script

--- scripts/rename_package.sh
+++ scripts/rename_package.sh
@@ -12,25 +12,45 @@ find . -type f -exec perl -pi -e "s/co\.daresay\.kmmtemplate/$new_package/g" {} 
 
 new_path=$(echo "$1" | tr "." "/")
 
-path="composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate"
+#path="composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate"
+#path1="shared/src/androidMain/kotlin/co/daresay/kmmtemplate"
+#
+## Construct the new path
+#new_full_path="composeApp/src/androidMain/kotlin/$new_path"
+#new_full_path1="shared/src/androidMain/kotlin/$new_path"
+#
+## Move the kmmtemplate folder and its contents
+#mkdir -p "$new_full_path"
+#mkdir -p "$new_full_path1"
+#
+## Move all files and subfolders
+#rsync -av "$path"/ "$new_full_path"
+#rsync -av "$path1"/ "$new_full_path1"
+#
+## Delete the 'co' folder and its contents
+#rm -rf "composeApp/src/androidMain/kotlin/co" || echo "Error deleting 'co' folder"
+#rm -rf "shared/src/androidMain/kotlin/co" || echo "Error deleting 'co' folder"
 
-# Construct the new path
-new_full_path="composeApp/src/androidMain/kotlin/$new_path"
+################
+# Define arrays for paths
+paths=("composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/commonMain/kotlin/co/daresay/kmmtemplate" "shared/src/iosMain/kotlin/co/daresay/kmmtemplate")
+new_paths=("composeApp/src/androidMain/kotlin/$new_path" "shared/src/androidMain/kotlin/$new_path" "shared/src/commonMain/kotlin/$new_path" "shared/src/iosMain/kotlin/$new_path")
+delete_paths=("composeApp/src/androidMain/kotlin/co" "shared/src/androidMain/kotlin/co" "shared/src/commonMain/kotlin/co" "shared/src/iosMain/kotlin/co")
 
-# Move the kmmtemplate folder and its contents
-mkdir -p "$new_full_path"
-ls -la
-echo "$new_full_path"
-echo "$path"
-#mv "$path" "$new_full_path"
-# Move all files and subfolders
-rsync -av "$path"/ "$new_full_path"
-# Extract the part to be removed
-#remove_part="${path%/*co*}"
+# Loop through the paths and perform operations
+for i in "${!paths[@]}"; do
+    path="${paths[$i]}"
+    new_full_path="${new_paths[$i]}"
+    delete_path="${delete_paths[$i]}"
 
-# Remove the folders if they exist
-#if [ -d "$remove_part" ]; then
-#  rm -rf "$remove_part"
-#fi
+    # Move the kmmtemplate folder and its contents
+    mkdir -p "$new_full_path"
+
+    # Move all files and subfolders
+    rsync -av "$path"/ "$new_full_path"
+
+    # Delete the 'co' folder and its contents
+    rm -rf "$delete_path" || echo "Error deleting '$delete_path' folder"
+done
 
 exit 0
