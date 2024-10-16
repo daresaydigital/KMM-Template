e67b8d6787f1d0e502b69a5d0d66175661136ebc
Yauheni <yauheni.slizh@knightec.se>
2024-10-16 12:09:38 +0200

    Updated script

--- scripts/rename_package.sh
+++ scripts/rename_package.sh
@@ -13,9 +13,9 @@ find . -type f -exec perl -pi -e "s/co\.daresay\.kmmtemplate/$new_package/g" {} 
 new_path=$(echo "$1" | tr "." "/")
 
 # Define arrays for paths
-paths=("composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/commonMain/kotlin/co/daresay/kmmtemplate" "shared/src/iosMain/kotlin/co/daresay/kmmtemplate" "shared/src/commonMain/sqldelight/co/daresay/kmmtemplate")
-new_paths=("composeApp/src/androidMain/kotlin/$new_path" "shared/src/androidMain/kotlin/$new_path" "shared/src/commonMain/kotlin/$new_path" "shared/src/iosMain/kotlin/$new_path" "shared/src/commonMain/sqldelight/$new_path")
-delete_paths=("composeApp/src/androidMain/kotlin/co" "shared/src/androidMain/kotlin/co" "shared/src/commonMain/kotlin/co" "shared/src/iosMain/kotlin/co" "shared/src/commonMain/sqldelight/co")
+paths=("composeApp/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/androidMain/kotlin/co/daresay/kmmtemplate" "shared/src/commonMain/kotlin/co/daresay/kmmtemplate" "shared/src/iosMain/kotlin/co/daresay/kmmtemplate" "shared/src/commonMain/sqldelight/co/daresay/kmmtemplate" "composeApp/src/commonMain/kotlin/co/daresay/kmmtemplate" "composeApp/src/iosMain/kotlin/co/daresay/kmmtemplate" "composeApp/src/commonMain/sqldelight/co/daresay/kmmtemplate")
+new_paths=("composeApp/src/androidMain/kotlin/$new_path" "shared/src/androidMain/kotlin/$new_path" "shared/src/commonMain/kotlin/$new_path" "shared/src/iosMain/kotlin/$new_path" "shared/src/commonMain/sqldelight/$new_path" "composeApp/src/commonMain/kotlin/co" "composeApp/src/iosMain/kotlin/$new_path" "composeApp/src/commonMain/sqldelight/$new_path")
+delete_paths=("composeApp/src/androidMain/kotlin/co" "shared/src/androidMain/kotlin/co" "shared/src/commonMain/kotlin/co" "shared/src/iosMain/kotlin/co" "shared/src/commonMain/sqldelight/co" "composeApp/src/commonMain/kotlin/$new_path" "composeApp/src/iosMain/kotlin/co" "composeApp/src/commonMain/sqldelight/co")
 
 # Loop through the paths and perform operations
 for i in "${!paths[@]}"; do
