6255f864682c05367dc1c30730f7569e64777e4c
Yauheni Slizh <tyteishi@gmail.com>
2024-10-30 10:57:55 +0100

    Updated script

--- scripts/add_dependencies.sh
+++ scripts/add_dependencies.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+# Get branch names from parameters
+branch1="$1"
+branch2="$2"
+
 # Get the diff output and extract dependency lines
-diff_output=$(git diff compose_swift_ui ktor_swift_ui -- shared/build.gradle.kts)
+diff_output=$(git diff "$branch1" "$branch2" -- shared/build.gradle.kts)
 echo "-----------------------------"
 echo "Diff output:"
 echo "$diff_output"
 echo "-----------------------------"
 
-
 # Function to extract and add dependencies/plugins for a specific block
 add_lines() {
   local target_block="$1"
   file_path="shared/build.gradle.kts"
+  echo "$file_path"
   local lines=$(echo "$diff_output" | sed -n "/$target_block/,/}/p" | grep "^+" | sed 's/^+//' | sed 's/\\/\\\\/g; s/&/\\&/g')
 
   # Check if the target block is empty
   if grep -q "$target_block {[\t ]*}" shared/build.gradle.kts; then
     # If empty, add a newline before the closing brace
+    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
     sed -i "/$target_block {/a \\" shared/build.gradle.kts
   fi
 
   while IFS= read -r line; do
-    sed -i "" "/$target_block {/,/}/s/$target_block {/$target_block {\n$line/1" "$file_path"
+    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
+    sed -i "/$target_block {/,/}/s/$target_block {/$target_block {\n$line/1" "$file_path"
   done <<< "$lines"
 }
 
@@ -36,7 +42,8 @@ file_path="shared/build.gradle.kts"
 while IFS= read -r plugin; do
     echo "-----------------------------"
     echo "Adding plugin: $plugin"
-    sed -i "" "/plugins {/,/}/s/plugins {/plugins {\n$plugin/1" "$file_path"
+    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
+    sed -i "/plugins {/,/}/s/plugins {/plugins {\n$plugin/1" shared/build.gradle.kts
     echo "-----------------------------"
 done <<< "$plugins"
 
