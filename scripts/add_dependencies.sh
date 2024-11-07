617e367f59d1649d4da8aa1984fa811d2394f302
Yauheni Slizh <tyteishi@gmail.com>
2024-11-07 11:40:17 +0100

    Update script to be able merge branches for compose setup

--- scripts/add_dependencies.sh
+++ scripts/add_dependencies.sh
@@ -4,8 +4,15 @@
 branch1="$1"
 branch2="$2"
 
+# Determine the file path based on the branch1 value
+if [[ "$branch1" == "compose_swift_ui" ]]; then
+  file_path="shared/build.gradle.kts"
+else
+  file_path="composeApp/build.gradle.kts"
+fi
+
 # Get the diff output and extract dependency lines
-diff_output=$(git diff "$branch1" "$branch2" -- shared/build.gradle.kts)
+diff_output=$(git diff "$branch1" "$branch2" -- "$file_path")
 echo "-----------------------------"
 echo "Diff output:"
 echo "$diff_output"
@@ -14,19 +21,15 @@ echo "-----------------------------"
 # Function to extract and add dependencies/plugins for a specific block
 add_lines() {
   local target_block="$1"
-  file_path="shared/build.gradle.kts"
-  echo "$file_path"
   local lines=$(echo "$diff_output" | sed -n "/$target_block/,/}/p" | grep "^+" | sed 's/^+//' | sed 's/\\/\\\\/g; s/&/\\&/g')
 
   # Check if the target block is empty
-  if grep -q "$target_block {[\t ]*}" shared/build.gradle.kts; then
+  if grep -q "$target_block {[\t ]*}" "$file_path"; then
     # If empty, add a newline before the closing brace
-    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
-    sed -i "/$target_block {/a \\" shared/build.gradle.kts
+    sed -i "/$target_block {/a \\" "$file_path"
   fi
 
   while IFS= read -r line; do
-    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
     sed -i "/$target_block {/,/}/s/$target_block {/$target_block {\n$line/1" "$file_path"
   done <<< "$lines"
 }
@@ -37,14 +40,12 @@ echo "-----------------------------"
 echo "Extracted plugins:"
 echo "$plugins"
 echo "-----------------------------"
-file_path="shared/build.gradle.kts"
 
 while IFS= read -r plugin; do
-    echo "-----------------------------"
-    echo "Adding plugin: $plugin"
-    # to run this command on iOS you have to add "" after -i. For instance, sed -i "/$target_block {/a \\" shared/build.gradle.kts
-    sed -i "/plugins {/,/}/s/plugins {/plugins {\n$plugin/1" shared/build.gradle.kts
-    echo "-----------------------------"
+  echo "-----------------------------"
+  echo "Adding plugin: $plugin"
+  sed -i "/plugins {/,/}/s/plugins {/plugins {\n$plugin/1" "$file_path"
+  echo "-----------------------------"
 done <<< "$plugins"
 
 # Add dependencies for each target block
@@ -53,7 +54,7 @@ add_lines "commonMain.dependencies"
 add_lines "iosMain.dependencies"
 
 # Extract remaining diff lines that are not part of the plugins or dependencies blocks
-remaining_diff=$(echo "$diff_output" | grep "^+" | grep -v "alias" | grep -v "implementation" | grep -v "++ b/shared/build.gradle.kts" | sed 's/^+//')
+remaining_diff=$(echo "$diff_output" | grep "^+" | grep -v "alias" | grep -v "implementation" | grep -v "++ b/$file_path" | sed 's/^+//')
 
 # Append remaining diff lines to the end of the file
 if [ -n "$remaining_diff" ]; then
@@ -61,11 +62,11 @@ if [ -n "$remaining_diff" ]; then
   echo "Appending remaining diff to the end of the file:"
   echo "$remaining_diff"
   echo "-----------------------------"
-  echo "$remaining_diff" >> shared/build.gradle.kts
+  echo "$remaining_diff" >> "$file_path"
 fi
 
 # Remove duplicate lines containing 'alias' or 'implementation' from the file
-awk '/alias|implementation/ { if (!seen[$0]++) print; next } { print }' shared/build.gradle.kts > temp && mv temp shared/build.gradle.kts
+awk '/alias|implementation/ { if (!seen[$0]++) print; next } { print }' "$file_path" > temp && mv temp "$file_path"
 
 echo "-----------------------------"
 echo "Dependencies, plugins, and remaining diff added successfully."
